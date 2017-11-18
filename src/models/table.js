1a6068a1a77d98fe0477529557aef07f1a31567a
YZ <1150681747@qq.com>
2017-11-19 01:04:31 +0800

    完成三张报表，差日期切换及打印 导出

--- src/models/table.js
+++ src/models/table.js
@@ -1,7 +1,7 @@
 /**
  * Created by YZ on 2017/11/15.
  */
-import * as tableService from '../services/table';
+import * as tableService from '../services/tables';
 
 export default {
   namespace: 'tables',
@@ -17,7 +17,18 @@ export default {
         balancesheetData,
       };
     },
-
+    fillProfit(state, { payload: profitData }) {
+      return {
+        ...state,
+        profitData,
+      };
+    },
+    fillCashflow(state, { payload: cashflowData }) {
+      return {
+        ...state,
+        cashflowData,
+      };
+    },
   },
   effects: {
     *fetchBalanceSheet({ payload }, { call, put }) {
@@ -26,9 +37,33 @@ export default {
         payload: null,
       });
       const data = yield call(tableService.fetchBalanceSheet, payload);
-      console.log(payload);
+      console.log(data);
       yield put({
         type: 'fillBalanceSheet',
+        payload: data,
+      });
+    },
+    *fetchProfit({ payload }, { call, put }) {
+      yield put({
+        type: 'fillProfit',
+        payload: null,
+      });
+      const data = yield call(tableService.fetchProfit, payload);
+      console.log(data);
+      yield put({
+        type: 'fillProfit',
+        payload: data,
+      });
+    },
+    *fetchCashflow({ payload }, { call, put }) {
+      yield put({
+        type: 'fillCashflow',
+        payload: null,
+      });
+      const data = yield call(tableService.fetchCashFlow, payload);
+      console.log(data);
+      yield put({
+        type: 'fillCashflow',
         payload: data,
       });
     },
@@ -38,6 +73,20 @@ export default {
       return history.listen(({ pathname, query }) => {
         dispatch({
           type: 'fetchBalanceSheet',
+          payload: {
+            companyID: 1,
+            phase: { phase: '2017-10' },
+          },
+        });
+        dispatch({
+          type: 'fetchProfit',
+          payload: {
+            companyID: 1,
+            phase: { phase: '2017-10' },
+          },
+        });
+        dispatch({
+          type: 'fetchCashflow',
           payload: {
             companyID: 1,
             phase: { phase: '2017-10' },
