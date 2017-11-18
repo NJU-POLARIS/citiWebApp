4f00c589ecde29f72d392f20181cdc3f9da31c6a
Molloh <mollohchiao@hotmail.com>
2017-11-18 13:19:33 +0800

    connect to server

--- src/common/nav.js
+++ src/common/nav.js
@@ -60,18 +60,6 @@ const data = [{
       name: '财务预警',
       path: 'warning',
       // component: Monitor,
-    }, {
-      name: '分析页',
-      path: 'analysis',
-      component: Analysis,
-    }, {
-      name: '监控页',
-      path: 'monitor',
-      component: Monitor,
-    }, {
-      name: '工作台',
-      path: 'workplace',
-      component: Workplace,
     }],
   }, {
     name: '财务管理',
@@ -86,6 +74,16 @@ const data = [{
       name: '账簿',
       icon: 'book',
       path: 'book',
+      children: [
+        {
+          path: 'confirm',
+          // component: dynamicWrapper(app, ['form'], import('../routes/Forms/StepForm/Step2')),
+        },
+        {
+          path: 'result',
+          // component: dynamicWrapper(app, ['form'], import('../routes/Forms/StepForm/Step3')),
+        },
+      ],
     }, {
       name: '报表',
       icon: 'file-text',
@@ -129,106 +127,10 @@ const data = [{
       name: '个人设置',
       path: 'DataManagement',
       component: DataManagement,
-    },{
-		name:'期初设置',
-		path:'InitialSetting',
-		component: InitialSetting,
-	}],
-  }, {
-    name: '表单页',
-    path: 'form',
-    icon: 'form',
-    children: [{
-      name: '基础表单',
-      path: 'basic-form',
-      component: BasicForm,
     }, {
-      name: '分步表单',
-      path: 'step-form',
-      component: StepForm,
-      children: [{
-        path: 'confirm',
-        component: Step2,
-      }, {
-        path: 'result',
-        component: Step3,
-      }],
-    }, {
-      name: '高级表单',
-      path: 'advanced-form',
-      component: AdvancedForm,
-    }],
-  }, {
-    name: '列表页',
-    path: 'list',
-    icon: 'table',
-    children: [{
-      name: '查询表格',
-      path: 'table-list',
-      component: TableList,
-    }, {
-      name: '标准列表',
-      path: 'basic-list',
-      component: BasicList,
-    }, {
-      name: '卡片列表',
-      path: 'card-list',
-      component: CardList,
-    }, {
-      name: '搜索列表（项目）',
-      path: 'cover-card-list',
-      component: CoverCardList,
-    }, {
-      name: '搜索列表（应用）',
-      path: 'filter-card-list',
-      component: FilterCardList,
-    }, {
-      name: '搜索列表（文章）',
-      path: 'search',
-      component: SearchList,
-    }],
-  }, {
-    name: '详情页',
-    path: 'profile',
-    icon: 'profile',
-    children: [{
-      name: '基础详情页',
-      path: 'basic',
-      component: BasicProfile,
-    }, {
-      name: '高级详情页',
-      path: 'advanced',
-      component: AdvancedProfile,
-    }],
-  }, {
-    name: '结果',
-    path: 'result',
-    icon: 'check-circle-o',
-    children: [{
-      name: '成功',
-      path: 'success',
-      component: Success,
-    }, {
-      name: '失败',
-      path: 'fail',
-      component: Error,
-    }],
-  }, {
-    name: '异常',
-    path: 'exception',
-    icon: 'warning',
-    children: [{
-      name: '403',
-      path: '403',
-      component: Exception403,
-    }, {
-      name: '404',
-      path: '404',
-      component: Exception404,
-    }, {
-      name: '500',
-      path: '500',
-      component: Exception500,
+      name: '期初设置',
+      path: 'InitialSetting',
+      component: InitialSetting,
     }],
   }],
 }, {
@@ -252,15 +154,6 @@ const data = [{
       component: RegisterResult,
     }],
   }],
-}, {
-  component: BlankLayout,
-  layout: 'BlankLayout',
-  children: {
-    name: '使用文档',
-    path: 'http://pro.ant.design/docs/getting-started',
-    target: '_blank',
-    icon: 'book',
-  },
 }];
 
 export function getNavData() {
