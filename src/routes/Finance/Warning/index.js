339bbdd9e861eb74f1be48fd7e186f7f1936a7e0
Molloh <mollohchiao@hotmail.com>
2017-11-23 11:07:41 +0800

    fix proxy

--- src/routes/Finance/Warning/index.js
+++ src/routes/Finance/Warning/index.js
@@ -1,6 +1,13 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
 
+import numeral from 'numeral';
+import { Row, Col, Card, Tooltip, Icon } from 'antd';
+
+import { Pie, WaterWave, Gauge, TagCloud, ChartCard, yuan, MiniArea, MiniBar, MiniProgress, Field, Bar, TimelineChart } from '../../../components/Charts';
+import NumberInfo from '../../../components/NumberInfo';
+import CountDown from '../../../components/CountDown';
+
 @connect(state => ({
   warning: state.warning,
 }))
@@ -10,15 +17,109 @@ class Warning extends PureComponent {
       {
         type: 'warning/queryWarning',
         payload: {
-
         }
       }
     );
   }
 
   render() {
+    const topColResponsiveProps = {
+      xs: 24,
+      sm: 12,
+      md: 12,
+      lg: 12,
+      xl: 6,
+      style: { marginBottom: 24 },
+
+    };
+
     return (
-      <div>Warning</div>
+      <div>
+        <Row gutter={24}>
+          <Col {...topColResponsiveProps}>
+            <ChartCard
+              bordered={false}
+              title="盈利能力"
+              action={<Tooltip title="盈利能力"><Icon type="info-circle-o" /></Tooltip>}
+              total="78%"
+              contentHeight={46}
+              footer={<Field label="日访问量" value={numeral(1234).format('0,0')} />}
+            >
+              <MiniProgress percent={78} strokeWidth={8} target={80} color="#13C2C2" />
+            </ChartCard>
+          </Col>
+          <Col {...topColResponsiveProps}>
+            <ChartCard
+              bordered={false}
+              title="偿债能力"
+              action={<Tooltip title="偿债能力"><Icon type="info-circle-o" /></Tooltip>}
+              total="78%"
+              contentHeight={46}
+              footer={<Field label="日访问量" value={numeral(1234).format('0,0')} />}
+            >
+              <MiniProgress percent={78} strokeWidth={8} target={80} color="#13C2C2" />
+            </ChartCard>
+          </Col>
+          <Col {...topColResponsiveProps}>
+            <ChartCard
+              bordered={false}
+              title="营运能力"
+              action={<Tooltip title="营运能力"><Icon type="info-circle-o" /></Tooltip>}
+              total="78%"
+              contentHeight={46}
+              footer={<Field label="日访问量" value={numeral(1234).format('0,0')} />}
+            >
+              <MiniProgress percent={78} strokeWidth={8} target={80} color="#13C2C2" />
+            </ChartCard>
+          </Col>
+          <Col {...topColResponsiveProps}>
+            <ChartCard
+              bordered={false}
+              title="成长能力"
+              action={<Tooltip title="成长能力"><Icon type="info-circle-o" /></Tooltip>}
+              total="78分"
+              contentHeight={46}
+              footer={<Field label="日访问量" value={numeral(1234).format('0,0')} />}
+            >
+              <MiniProgress percent={78} strokeWidth={8} target={80} color="#13C2C2" />
+            </ChartCard>
+          </Col>
+        </Row>
+        <Card
+          bordered={false}
+          bodyStyle={{ padding: 0 }}
+        >
+          <Row gutter={24}>
+            <Col xl={16} lg={12} md={12} sm={24} xs={24}>
+              <div>
+                <Gauge
+                  format={(val) => {
+                    switch (parseInt(val, 10)) {
+                      case 50:
+                        return '高度风险';
+                      case 80:
+                        return '中高风险';
+                      case 100:
+                        return '中度风险';
+                      case 120:
+                        return '中低风险';
+                      case 150:
+                        return '低度风险';
+                      default:
+                        return '';
+                    }
+                  }}
+                  title="跳出率"
+                  height={180}
+                  percent={87}
+                />
+              </div>
+            </Col>
+            <Col xl={8} lg={12} md={12} sm={24} xs={24}>
+            </Col>
+          </Row>
+        </Card>
+      </div>
     );
   }
 }
